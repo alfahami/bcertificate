8e70d9f93e5a3a8d943241104bef892b60737d6c
AL FAHAMI TOIHIR <altoihir@gmail.com>
2024-05-14 10:30:05 +0100

    refractor: delete separate folder structure

--- certificate/java/loader.java
+++
@@ -1,8 +0,0 @@
-import java.util.Objects;
-
-public class Main {
-  public static void main(String[] args) {
-    String[] myStr = {"ALFAHAMI TOIHIR", "18013942", "NBE388507", "21/05/1992", "MORONI", "MASTER GLCL", "Assez-bien", "07/12/2021"};
-    System.out.println(Integer.toHexString(Objects.hashCode(myStr)));
-  }
-}
